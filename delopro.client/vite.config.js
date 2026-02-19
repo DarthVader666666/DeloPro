f6f2517c9e7cd71717e965f9a30dc0c95307ce7f
DarthVader666666 <rumyancer@gmail.com>
2026-02-19 20:02:06 +0300

    Implemented mode

--- delopro.client/vite.config.js
+++ delopro.client/vite.config.js
@@ -10,48 +10,55 @@ const certificateName = "delopro.client";
 const certFilePath = `${certificateName}.pem`;
 const keyFilePath = `${certificateName}.key`;
 
-const isDev = env.VITE_API_ENVIRONMENT === 'development'
-console.log(env.VITE_API_ENVIRONMENT)
-if(isDev) {
-  if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-  }
-}
-
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7250';
 
-export default defineConfig({
-    plugins: [plugin()],
-    appType: 'spa',
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+  console.log("ENVIRONMENT: " + mode)
+  if (isDev) {
+    if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+      const result = child_process.spawnSync('dotnet',
+        [
+          'dev-certs',
+          'https',
+          '--export-path',
+          certFilePath,
+          '--format',
+          'Pem',
+          '--no-password',
+        ],
+         { stdio: 'inherit' })
+
+        if (result.status !== 0) {
+          throw new Error("Could not create certificate.")
         }
-    },
-    server: isDev ? {
-        proxy: {
-            '^/delopro': {
-                target,
-                secure: false
-            }
-        },
-        port: 5173,
-        https: {
-          key: fs.readFileSync(keyFilePath),
-          cert: fs.readFileSync(certFilePath),
-        }
+      }
     }
-    : undefined ,
-    build: { chunkSizeWarningLimit: 2000 }
-})
+
+    return {
+      plugins: [plugin()],
+      appType: 'spa',
+      resolve: {
+        alias: { '@': fileURLToPath(new URL('./src', import.meta.url))
+
+        }
+      },
+      server: isDev
+        ? {
+            proxy: {
+               '^/delopro': {
+                 target,
+                 secure: false
+              }
+            },
+            port: 5173,
+            https: {
+              key: fs.readFileSync(keyFilePath), cert: fs.readFileSync(certFilePath),
+            }
+          } : undefined,
+          build: {
+            chunkSizeWarningLimit: 2000
+          }
+        }
+      })
